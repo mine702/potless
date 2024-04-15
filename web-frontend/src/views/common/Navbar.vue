4e4566cb4cec7d075376fa89c4e61099c2ea298b
차준석 <wnstjr401@naver.com>
2024-04-15 16:27:42 +0900

    feat : navbar 추가

---
+++ web-frontend/src/views/common/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="navbar-container">
+    <div class="logo">서비스 로고</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.navbar-container {
+  background: #cdc9c9;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
