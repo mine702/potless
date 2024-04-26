c0048bc43cd459ce42ed2b3f10587ad2ec951ee6
CHAEEUN LEE <cheeun0903@gmail.com>
2024-04-26 16:04:49 +0900

    feat : 네브바 디자인 - 메인에 로고명과 로그아웃/로그인 버튼 - 서브에 포트홀 조회, 작업 정보, 통계 자료 link - link에 밑줄 오른쪽에서 왼쪽으로 그어지는 애니메이션 추가 - 로그아웃/로그인 버튼 호버시 색상 변경

--- web-frontend/src/views/common/Navbar.vue
+++ web-frontend/src/views/common/Navbar.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="navbar-container">
-    <div class="logo" @click="store.moveHome">서비스 로고</div>
-    <div class="move-page">
-      <div class="porthole" @click="store.movePorthole">포트홀 조회</div>
-      <div class="path" @click="store.movePath">공사 추천 경로</div>
-      <div class="task" @click="store.moveTask">작업 정보</div>
-      <div class="statistics" @click="store.moveStatistics">통계 자료</div>
-      <button class="logout" @click="store.moveLogin">로그아웃</button>
+  <div>
+    <div class="main-navbar">
+      <div class="logo text-title" @click="store.moveHome">POTLESS</div>
+      <!-- <button class="logout text-title" @click="store.moveLogin">
+        <img src="@/assets/icon/enter.png" alt="#" />
+        <div>로그인</div>
+      </button> -->
+      <button class="logout text-title" @click="store.moveLogin">
+        <img src="@/assets/icon/out.png" alt="#" />
+        <div>로그아웃</div>
+      </button>
+    </div>
+
+    <div class="sub-navbar">
+      <div class="text-dark-title nav-item first-item" @click="store.movePorthole">포트홀 조회</div>
+      <div class="text-dark-title nav-item" @click="store.moveTask">작업 정보</div>
+      <div class="text-dark-title nav-item" @click="store.moveStatistics">통계 자료</div>
     </div>
   </div>
 </template>
@@ -18,25 +27,83 @@ const store = useMoveStore();
 </script>
 
 <style scoped>
-.navbar-container {
-  background: #cdc9c9;
+.text-light-title {
+  color: #1d1d1d;
+}
+
+.text-dark-title {
+  color: white;
+}
+
+.main-navbar {
+  background: #ffffff;
   width: 100%;
-  height: 70px;
+  height: 75px;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
+
 .logo {
-  margin: 0 20px;
+  margin-left: 30px;
+  font-size: 30px;
+  font-weight: bold;
+  cursor: pointer;
 }
-.move-page {
-  display: flex;
-}
-.move-page > div {
-  margin: 0 20px;
-}
+
 .logout {
-  height: 50%;
-  margin: 0 20px;
+  height: 70%;
+  margin-right: 30px;
+  padding: 0px 12px 0px 12px;
+  font-size: 22px;
+  font-weight: bold;
+  background-color: white;
+  border: none;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+}
+
+.logout:hover {
+  background-color: #151b621c;
+}
+
+.sub-navbar {
+  background: #151c62;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+}
+
+.nav-item {
+  margin-right: 70px;
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
+  position: relative;
+  padding-bottom: 3px;
+  overflow: hidden;
+}
+
+.nav-item::after {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 1.5px;
+  background-color: white;
+  transform: scaleX(0);
+  transform-origin: left;
+  transition: transform 0.2s ease-out;
+}
+
+.nav-item:hover::after {
+  transform: scaleX(1);
+}
+
+.first-item {
+  margin-left: 32px;
 }
 </style>
