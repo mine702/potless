c9931f363464f0ec2a544468d43574382cd0f590
차준석 <wnstjr401@naver.com>
2024-04-28 13:55:48 +0900

    feat : pdf 변환 기능 추가

--- web-frontend/src/views/task/TaskInfoDetail.vue
+++ web-frontend/src/views/task/TaskInfoDetail.vue
@@ -6,26 +6,81 @@
       <div>담당자 : {{ taskHeader.managerName }}</div>
       <div>작업 건수: {{ taskHeader.projectSize }} 건</div>
       <div>작업 일자: {{ taskHeader.projectDate }}</div>
-      <button>PDF로 변환하기</button>
+      <button @click="generatePdf">PDF로 변환하기</button>
     </div>
     <List :data="taskData" />
+    <div id="pdf" class="report-pdf">
+      <PDFGeneratorMain
+        :task-header="taskHeader"
+        ref="documentRef"
+        class="pdf"
+        id="pdf"
+      />
+      <div v-for="pothole in taskData" :key="pothole.id">
+        <PDFGeneratorDetail :pothole="pothole" ref="documentRef" class="pdf" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import html2pdf from "html2pdf.js";
+import { ref, onMounted } from "vue";
 import List from "./components/List.vue";
 import data from "./DummyData.json";
+import PDFGeneratorMain from "./components/PDFGeneratorMain.vue";
+import PDFGeneratorDetail from "./components/PDFGeneratorDetail.vue";
 
 const taskHeader = ref(data.projectResponse);
 const taskData = ref(data.projectDetailResponse);
+const documentRef = ref(null);
+
+onMounted(() => {
+  onMounted(() => {
+    if (documentRef.value) {
+      console.log("Document ref is available:", documentRef.value);
+    } else {
+      console.error("Document ref is not available");
+    }
+  });
+});
+
+function generatePdf() {
+  const pdfArea = document.getElementById("pdf");
+  pdfArea.style.display = "block";
+
+  const options = {
+    margin: 0,
+    filename: "downloaded.pdf",
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 2 },
+  };
+
+  html2pdf()
+    .set(options)
+    .from(pdfArea)
+    .save()
+    .then(() => {
+      pdfArea.style.display = "none";
+    });
+}
 </script>
 
 <style scoped>
+.task-detail-container {
+  box-sizing: border-box;
+  overflow: hidden;
+  max-height: 100vh;
+}
 .header {
   display: flex;
   justify-content: space-between;
   padding: 20px;
   background-color: #f0f0f0;
+}
+
+.report-pdf {
+  margin: 0%;
+  padding: 0%;
 }
 </style>
---
+++ web-frontend/src/views/task/components/PDFGeneratorDetail.vue
@@ -0,0 +1,191 @@
+<template>
+  <div class="pdf-container">
+    <div class="header">
+      <div class="pothole-info">
+        <div class="title">위험물 정보</div>
+        <div class="info-i">
+          위험물 No. <span class="numebr">{{ props.pothole.damageId }}</span>
+        </div>
+        <div class="info">
+          <div>위험물 유형:</div>
+          <div>
+            {{ props.pothole.dtype }}
+          </div>
+        </div>
+        <div class="info">
+          <div>심각도:</div>
+          <div :style="severityStyle">
+            {{
+              props.pothole.severity === 1
+                ? "안전"
+                : props.pothole.severity === 2
+                ? "위험"
+                : props.pothole.severity === 3
+                ? "심각"
+                : "Unknown"
+            }}
+          </div>
+        </div>
+        <div class="info">
+          <div>너비:</div>
+          <div>{{ props.pothole.width }} mm</div>
+        </div>
+      </div>
+      <div ref="mapContainer" class="img-container">
+        <img :src="imageUrl" alt="" />
+      </div>
+    </div>
+    <div class="middle">
+      <div class="info">
+        <div>상세 주소:</div>
+        <div>
+          {{ props.pothole.location }} {{ props.pothole.area }}
+          {{ props.pothole.roadName }}
+        </div>
+      </div>
+      <div class="direct info">
+        <div>위경도 좌표:</div>
+        <div class="coordinates">
+          <div>x: {{ props.pothole.dir_x }}</div>
+          <div>y: {{ props.pothole.dir_y }}</div>
+        </div>
+      </div>
+      <div class="info">
+        <div>마지막 탐지 일시:</div>
+        <div>
+          {{ props.pothole.createAt }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref, computed } from "vue";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import leafletImage from "leaflet-image";
+import marker from "../../../assets/icon/marker.png";
+
+const mapContainer = ref(null);
+const imageUrl = ref("");
+
+const props = defineProps({
+  pothole: Object,
+});
+
+const severityStyle = computed(() => {
+  switch (props.pothole.severity) {
+    case 1:
+      return { color: "green" };
+    case 2:
+      return { color: "blue" };
+    case 3:
+      return { color: "red" };
+    default:
+      return { color: "black" };
+  }
+});
+
+function preloadImage(url) {
+  let img = new Image();
+  img.onload = function () {
+    console.log("Image preloaded successfully");
+  };
+  img.onerror = function () {
+    console.error("Failed to load image: Image loading failed");
+  };
+  img.src = url;
+}
+
+onMounted(() => {
+  const map = L.map(mapContainer.value, {
+    zoomControl: false,
+  }).setView([props.pothole.dir_x, props.pothole.dir_y], 16);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap contributors",
+    maxZoom: 19,
+  }).addTo(map);
+
+  const icon = L.icon({
+    iconUrl: marker,
+    iconSize: [32, 32],
+    iconAnchor: [16, 16],
+  });
+
+  L.marker([props.pothole.dir_x, props.pothole.dir_y], { icon }).addTo(map);
+
+  map.whenReady(() => {
+    leafletImage(map, function (err, canvas) {
+      if (err) {
+        console.error("Failed to generate image from map:", err);
+        return;
+      }
+      imageUrl.value = canvas.toDataURL();
+      preloadImage(imageUrl.value);
+    });
+  });
+});
+</script>
+
+<style scoped>
+.pdf-container {
+  border: 1px solid black;
+  width: 210mm;
+  height: 297mm;
+  display: flex;
+  flex-direction: column;
+  padding: 70px;
+  box-sizing: border-box;
+  font-family: "돋움체";
+}
+
+.numebr {
+  color: blue;
+}
+.img-container {
+  border: 2px solid red;
+}
+.header {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  height: 300px;
+}
+
+.title {
+  font-size: 50px;
+  font-weight: 800;
+  margin-bottom: 20px;
+}
+.info-i {
+  width: 90%;
+  font-size: 30px;
+  margin-bottom: 5px;
+}
+.info {
+  width: 90%;
+  font-size: 30px;
+  margin-bottom: 5px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.direct {
+  display: flex;
+}
+.direct .coordinates {
+  display: flex;
+  flex-direction: column;
+}
+
+.direct .coordinates > div {
+  margin-bottom: 4px;
+}
+.middle {
+  margin-top: 40px;
+}
+.map {
+  width: 100%;
+  height: 100%;
+}
+</style>
