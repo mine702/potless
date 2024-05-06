774b4ca82f7d88f3bcf86095f820f351eb460d56
CHAEEUN LEE <cheeun0903@gmail.com>
2024-05-06 16:59:35 +0900

    feat : 작업팀 추가 디자인 - button, input css 변경 - 뒤로가기 버튼 추가 - 리스트 bullet point 추가 - 모달 사이즈 변경

--- web-frontend/src/views/porthole/components/AddTeamModal.vue
+++ web-frontend/src/views/porthole/components/AddTeamModal.vue
@@ -1,29 +1,27 @@
 <template>
-  <span class="close" @click="toggleModal">&times;</span>
-  <button
-    class="new-team-btn"
-    @click="isAddingTeam = true"
-    v-if="!isAddingTeam"
-  >
+  <div class="modal-header">
+    <span v-if="isAddingTeam" @click="goBack" class="back">&larr;</span>
+    <span v-else class="placeholder"></span>
+    <span class="close" @click="toggleModal">&times;</span>
+  </div>
+  <button class="new-team-btn" @click="isAddingTeam = true" v-if="!isAddingTeam">
     새로운 팀 추가
   </button>
   <div class="add-team-modal">
-    <div v-if="isAddingTeam">
-      <AddTeam
-        v-if="isAddingTeam"
-        :is-adding-team="isAddingTeam"
-        @update:isAddingTeam="updateIsAddingTeam"
-      />
-    </div>
-    <div v-else>
-      <div class="team-list" v-for="team in datas">
-        <div class="team-name">{{ team.teamName }}</div>
-        <div class="person" v-for="worker in team.workerList">
-          작업자 번호: {{ worker.memberId }} / 작업자 이름:
-          {{ worker.workerName }}
+    <div class="content-container">
+      <div v-if="isAddingTeam">
+        <AddTeam :is-adding-team="isAddingTeam" @update:isAddingTeam="updateIsAddingTeam" />
+      </div>
+      <div v-else>
+        <div class="team-list" v-for="team in datas">
+          <div class="team-name">{{ team.teamName }}</div>
+          <div class="person" v-for="worker in team.workerList">
+            작업자 번호: {{ worker.memberId }} / 작업자 이름:
+            {{ worker.workerName }}
+          </div>
+          <button @click="deleteTeamEvent(team.teamId)">팀 삭제</button>
+          <hr />
         </div>
-        <button @click="deleteTeamEvent(team.teamId)">팀 삭제</button>
-        <hr />
       </div>
     </div>
   </div>
@@ -85,29 +83,70 @@ function updateIsAddingTeam(value) {
   takeTeamList();
 }
 
+const goBack = () => {
+  isAddingTeam.value = false;
+};
+
 onMounted(() => {
   takeTeamList();
 });
 </script>
 
 <style scoped>
-.close {
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 2vh 0vh 0.5vh 0vh;
+}
+
+.back,
+.close,
+.placeholder {
   color: #aaa;
-  float: right;
-  font-size: 28px;
+  font-size: 4.5vh;
   font-weight: bold;
+  margin: -1vh 0vh;
+  cursor: pointer;
+}
+
+.placeholder {
+  visibility: hidden;
 }
 
 .close:hover,
-.close:focus {
-  color: black;
+.back:hover {
+  color: #7d7d7d;
   text-decoration: none;
   cursor: pointer;
 }
+
 .add-team-modal {
+  margin: 1vh 0px;
+  height: 57vh;
+  /* overflow-y: auto; */
+  /* border: 1px solid black; */
+}
+
+.team-list {
 }
 
 .new-team-btn {
-  margin-top: 15px;
+  background-color: #151c62;
+  padding: 0.9vh 0.7vw;
+  cursor: pointer;
+  border-radius: 4px;
+  font-size: 1.55vh;
+  position: relative;
+  overflow: hidden;
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  transition: all 0.3s;
+  margin-top: 2vh;
+}
+
+.new-team-btn:hover {
+  background-color: #0e1241;
 }
 </style>
