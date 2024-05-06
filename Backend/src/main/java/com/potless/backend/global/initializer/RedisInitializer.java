02edee6e278ff3844ce0c0e602d7a5968d59b6a5
최소영 <tiger6453127@naver.com>
2024-05-06 16:55:25 +0900

    fix: team - worker cascading delete 제거, createTeam로직 수정

--- Backend/src/main/java/com/potless/backend/global/initializer/RedisInitializer.java
+++ Backend/src/main/java/com/potless/backend/global/initializer/RedisInitializer.java
@@ -1,25 +1,25 @@
-package com.potless.backend.global.initializer;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.data.redis.core.RedisCallback;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
-
-@Component
-@Log4j2
-@RequiredArgsConstructor
-public class RedisInitializer implements CommandLineRunner {
-
-    private final RedisTemplate<String, String> redisTemplate;
-
-    @Override
-    public void run(String... args) throws Exception {
-        redisTemplate.execute((RedisCallback<? extends Object>) connection -> {
-            log.info("FlushAll");
-            connection.flushAll();
-            return null;
-        });
-    }
-}
+//package com.potless.backend.global.initializer;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.log4j.Log4j2;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.data.redis.core.RedisCallback;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Log4j2
+//@RequiredArgsConstructor
+//public class RedisInitializer implements CommandLineRunner {
+//
+//    private final RedisTemplate<String, String> redisTemplate;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        redisTemplate.execute((RedisCallback<? extends Object>) connection -> {
+//            log.info("FlushAll");
+//            connection.flushAll();
+//            return null;
+//        });
+//    }
+//}
