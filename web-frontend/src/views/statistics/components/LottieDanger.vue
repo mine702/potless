58ea29d2db776f6013de5ef33bbbd444c25da552
CHAEEUN LEE <cheeun0903@gmail.com>
2024-05-09 13:19:55 +0900

    feat : 통계 자료 디자인 - 타이틀 옆 로티 애니메이션 추가

---
+++ web-frontend/src/views/statistics/components/LottieDanger.vue
@@ -0,0 +1,25 @@
+<template>
+  <div ref="lottieContainer" style="width: 4%; height: 4%"></div>
+</template>
+
+<script setup>
+import { onMounted, onBeforeUnmount, ref } from "vue";
+import lottie from "lottie-web";
+
+const lottieContainer = ref(null);
+let anim;
+
+onMounted(() => {
+  anim = lottie.loadAnimation({
+    container: lottieContainer.value,
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "../../../../public/lottie/Danger.json",
+  });
+});
+
+onBeforeUnmount(() => {
+  anim.destroy();
+});
+</script>
