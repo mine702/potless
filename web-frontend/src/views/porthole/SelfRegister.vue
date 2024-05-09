30476b3205c9ab637cb140dd8427f42217d23920
차준석 <wnstjr401@naver.com>
2024-05-09 15:00:11 +0900

    feat : 수동 신고 로직 완성

--- web-frontend/src/views/porthole/SelfRegister.vue
+++ web-frontend/src/views/porthole/SelfRegister.vue
@@ -1,39 +1,31 @@
 <template>
   <div class="form-container">
-    <h1>포트홀 신고</h1>
-    <form @submit.prevent="submitForm">
-      <div class="input-group">
-        <label for="potholeType">포트홀 타입:</label>
-        <select id="potholeType" v-model="potholeType" required>
-          <option disabled value="">타입 선택</option>
-          <option>POTHOLE</option>
-          <option>CRACK</option>
-        </select>
+    <div class="header-box">
+      <h1>포트홀 신고</h1>
+      <div class="form-box">
+        <form @submit.prevent="submitForm" class="form-content">
+          <div class="input-group">
+            <select id="potholeType" v-model="potholeType" required>
+              <option disabled value="">타입 선택</option>
+              <option>POTHOLE</option>
+              <option>CRACK</option>
+            </select>
+          </div>
+          <div class="input-group">
+            <select id="severity" v-model="severity" required>
+              <option disabled value="">심각도 선택</option>
+              <option value="3">심각</option>
+              <option value="2">주의</option>
+              <option value="1">양호</option>
+            </select>
+          </div>
+          <button type="submit">신고하기</button>
+        </form>
       </div>
-      <div class="input-group">
-        <label for="severity">심각도:</label>
-        <select id="severity" v-model="severity" required>
-          <option disabled value="">심각도 선택</option>
-          <option value="3">심각</option>
-          <option value="2">주의</option>
-          <option value="1">양호</option>
-        </select>
-      </div>
-      <div class="input-group">
-        <label for="district">구:</label>
-        <input type="text" id="district" v-model="district" required />
-      </div>
-      <div class="input-group">
-        <label for="subDistrict">동:</label>
-        <input type="text" id="subDistrict" v-model="subDistrict" required />
-      </div>
-      <div class="input-group">
-        <label for="plotNumber">지번:</label>
-        <input type="text" id="plotNumber" v-model="plotNumber" required />
-      </div>
-
-      <button type="submit">신고하기</button>
-    </form>
+    </div>
+    <div class="map-box">
+      <SearchMap @updateCenter="handleCenterUpdate" />
+    </div>
   </div>
 </template>
 
@@ -43,20 +35,20 @@ import { postPotholeAdd } from "../../api/pothole/pothole";
 import { useAuthStore } from "@/stores/user";
 import { storeToRefs } from "pinia";
 import { useMoveStore } from "@/stores/move";
+import SearchMap from "./components/SearchMap.vue";
 
 const store = useMoveStore();
 const store2 = useAuthStore();
 const { accessToken } = storeToRefs(store2);
 const potholeType = ref("");
-const district = ref("");
-const subDistrict = ref("");
-const plotNumber = ref("");
-const severity = ref(null);
+const severity = ref("");
+const location_x = ref("");
+const location_y = ref("");
 
 const submitForm = () => {
-  const address = `${district.value} ${subDistrict.value} ${plotNumber.value}`;
   const potholeInfo = {
-    address: address,
+    x: location_x.value,
+    y: location_y.value,
     severity: Number(severity.value),
     type: potholeType.value,
   };
@@ -80,33 +72,51 @@ const submitForm = () => {
     }
   );
 };
+
+const handleCenterUpdate = (coords) => {
+  location_x.value = coords.x;
+  location_y.value = coords.y;
+  console.log("Map center updated to:", coords);
+};
 </script>
 
 <style scoped>
 .form-container {
-  max-width: 500px;
-  margin: 0 auto;
+  max-width: 900px;
+  margin: auto;
   padding: 20px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
+.header-box {
+  display: flex;
+  align-items: center; /* 세로 중앙 정렬 */
+}
+.form-box {
+  width: 500px;
+  margin-left: 20px;
+}
+.form-content {
+  display: flex;
+  align-items: center;
+}
 
 .input-group {
-  margin-bottom: 20px;
+  min-width: 150px;
+  height: 50px;
+}
+
+select,
+input,
+button {
+  margin: 10px;
+  width: 150px;
+  height: 100%; 
 }
 
 label {
   display: block;
   margin-bottom: 5px;
 }
-
-input,
-select,
-button {
-  width: 100%;
-  padding: 8px;
-  box-sizing: border-box;
-}
-
 button {
   background-color: #007bff;
   color: white;
@@ -114,8 +124,10 @@ button {
   cursor: pointer;
   transition: background-color 0.3s;
 }
-
 button:hover {
   background-color: #0056b3;
+}
+.map-box {
+  margin-top: 20px;
 }
 </style>
