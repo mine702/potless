1faa53b5d1d51d40eacbafd87953422fe57f273b
최소영 <tiger6453127@naver.com>
2024-05-14 15:14:12 +0900

    hotfix: setWidth

--- Backend/src/main/java/com/potless/backend/damage/service/AsyncService.java
+++ Backend/src/main/java/com/potless/backend/damage/service/AsyncService.java
@@ -95,6 +95,7 @@ public class AsyncService {
                                     .status(Status.작업전)
                                     .area(area)
                                     .location(location)
+                                    .width(damageSetRequestDTO.getWidth())
                                     .images(damageSetRequestDTO.getImages())
                                     .build();
 
