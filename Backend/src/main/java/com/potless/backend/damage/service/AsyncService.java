d5855b38191cd2ea1c8bb7a31f6bb10cf8c9eeff
최소영 <tiger6453127@naver.com>
2024-05-16 04:36:38 +0900

    feat: 2차 탐지 실패시 저장 x

--- Backend/src/main/java/com/potless/backend/damage/service/AsyncService.java
+++ Backend/src/main/java/com/potless/backend/damage/service/AsyncService.java
@@ -10,6 +10,7 @@ import com.potless.backend.damage.dto.service.response.kakao.RoadAddress;
 import com.potless.backend.damage.entity.enums.Status;
 import com.potless.backend.damage.repository.DamageRepository;
 import com.potless.backend.global.exception.pothole.DuplPotholeException;
+import com.potless.backend.global.exception.pothole.PotholeDetectionFailException;
 import com.potless.backend.global.exception.pothole.PotholeNotFoundException;
 import com.potless.backend.hexagon.repository.HexagonRepository;
 import com.potless.backend.hexagon.service.H3Service;
@@ -55,79 +56,89 @@ public class AsyncService {
             ReDetectionRequestDTO detectionRequestDTO = new ReDetectionRequestDTO(imageFile);
             ReDetectionResponseDTO detectionResult = detectionApiService.reDetectionResponse(detectionRequestDTO);
 
-            // 1차 탐지 후 BeforeVerification/ 에 사진 담기
-            String fileName = "BeforeVerification/" + System.currentTimeMillis() + "_" + imageFile.getName();
-            Map<String, String> fileUrlAndKey = awsService.uploadFileToS3(imageFile, fileName);
-            List<String> fileUrls = new ArrayList<>(fileUrlAndKey.values());
-            log.info("fileName = {}", fileName);
-            log.info("fileUrlAndKey = {}", fileUrlAndKey);
-            for (String fileUrl : fileUrls) {
-                log.info("fileUrl = {}", fileUrl);
+            // 2차 탐지가 되지 않았을경우 하위 로직 수행하지 않음
+            if(detectionResult.getSeverity() != 0) {
+                // 1차 탐지 후 BeforeVerification/ 에 사진 담기
+                String fileName = "BeforeVerification/" + System.currentTimeMillis() + "_" + imageFile.getName();
+                Map<String, String> fileUrlAndKey = awsService.uploadFileToS3(imageFile, fileName);
+                List<String> fileUrls = new ArrayList<>(fileUrlAndKey.values());
+                log.info("fileName = {}", fileName);
+                log.info("fileUrlAndKey = {}", fileUrlAndKey);
+                for (String fileUrl : fileUrls) {
+                    log.info("fileUrl = {}", fileUrl);
+                }
+
+                damageSetRequestDTO.setSeverity(detectionResult.getSeverity());
+                damageSetRequestDTO.setWidth((double) detectionResult.getWidth());
+
+                // 2차 탐지 성공하면 AfterVerification/BeforeWork/ 파일로 이동
+                List<String> newFileUrls = new ArrayList<>();
+                for (String fileUrl : fileUrls) {
+                    String destinationKey = "AfterVerification/BeforeWork/" + new File(fileUrl).getName();
+                    String newUrl = awsService.moveFileToVerified(fileName, destinationKey);
+                    newFileUrls.add(newUrl);
+                    log.info("fileUrl = {}", fileUrl);
+                    log.info("newUrl = {}", newUrl);
+                }
+
+                damageSetRequestDTO.setImages(newFileUrls);
+
+                // 비동기로 처리하고 바로 응답 반환 검증
+                kakaoService.fetchKakaoData(damageSetRequestDTO.getX(), damageSetRequestDTO.getY())
+                            .thenAcceptAsync(data -> {
+                                try {
+                                    RoadAddress roadAddress = data.getDocuments()
+                                                                  .get(0)
+                                                                  .getRoad_address();
+                                    Address address = data.getDocuments()
+                                                          .get(0)
+                                                          .getAddress();
+                                    String city = (address != null) ? address.getRegion_1depth_name() : roadAddress.getRegion_1depth_name();
+
+                                    String addressName = (address != null) ? address.getAddress_name() : roadAddress.getAddress_name();
+                                    String location;
+                                    String area;
+
+                                    if (city.equals("대전")) {
+                                        location = (address != null) ? address.getRegion_3depth_name() : "정보가 존재하지 않습니다";
+                                        area = (address != null) ? address.getRegion_2depth_name() : roadAddress.getRegion_2depth_name();
+                                    } else {
+                                        location = "기타";
+                                        area = "기타";
+                                    }
+
+                                    DamageSetRequestServiceDTO serviceDTO =
+                                            DamageSetRequestServiceDTO.builder()
+                                                                      .dirX(damageSetRequestDTO.getX())
+                                                                      .dirY(damageSetRequestDTO.getY())
+                                                                      .dtype(damageSetRequestDTO.getDtype())
+                                                                      .width(damageSetRequestDTO.getWidth())
+                                                                      .address(addressName)
+                                                                      .severity(damageSetRequestDTO.getSeverity())
+                                                                      .status(Status.작업전)
+                                                                      .area(area)
+                                                                      .location(location)
+                                                                      .width(damageSetRequestDTO.getWidth())
+                                                                      .images(damageSetRequestDTO.getImages())
+                                                                      .memberId(damageSetRequestDTO.getMemberId())
+                                                                      .build();
+
+                                    serviceDTO.setHexagonIndex(hexagonIndex);
+                                    iDamageService.setDamage(serviceDTO);
+                                } catch (Exception e) {
+                                    for (String s : newFileUrls)
+                                        awsService.deleteFile(s);
+                                    log.error("Error processing Kakao data: {}", e.getMessage());
+                                    throw new PotholeNotFoundException();
+                                }
+                            });
             }
-
-            damageSetRequestDTO.setSeverity(detectionResult.getSeverity());
-            damageSetRequestDTO.setWidth((double) detectionResult.getWidth());
-
-            // 2차 탐지 성공하면 AfterVerification/BeforeWork/ 파일로 이동
-            List<String> newFileUrls = new ArrayList<>();
-            for (String fileUrl : fileUrls) {
-                String destinationKey = "AfterVerification/BeforeWork/" + new File(fileUrl).getName();
-                String newUrl = awsService.moveFileToVerified(fileName, destinationKey);
-                newFileUrls.add(newUrl);
-                log.info("fileUrl = {}", fileUrl);
-                log.info("newUrl = {}", newUrl);
-            }
-
-            damageSetRequestDTO.setImages(newFileUrls);
-
-            // 비동기로 처리하고 바로 응답 반환 검증
-            kakaoService.fetchKakaoData(damageSetRequestDTO.getX(), damageSetRequestDTO.getY())
-                    .thenAcceptAsync(data -> {
-                        try {
-                            RoadAddress roadAddress = data.getDocuments().get(0).getRoad_address();
-                            Address address = data.getDocuments().get(0).getAddress();
-                            String city = (address != null) ? address.getRegion_1depth_name() : roadAddress.getRegion_1depth_name();
-
-                            String addressName = (address != null) ? address.getAddress_name() : roadAddress.getAddress_name();
-                            String location;
-                            String area;
-
-                            if (city.equals("대전")) {
-                                location = (address != null) ? address.getRegion_3depth_name() : "정보가 존재하지 않습니다";
-                                area = (address != null) ? address.getRegion_2depth_name() : roadAddress.getRegion_2depth_name();
-                            } else {
-                                location = "기타";
-                                area = "기타";
-                            }
-
-                            DamageSetRequestServiceDTO serviceDTO = DamageSetRequestServiceDTO.builder()
-                                    .dirX(damageSetRequestDTO.getX())
-                                    .dirY(damageSetRequestDTO.getY())
-                                    .dtype(damageSetRequestDTO.getDtype())
-                                    .width(damageSetRequestDTO.getWidth())
-                                    .address(addressName)
-                                    .severity(damageSetRequestDTO.getSeverity())
-                                    .status(Status.작업전)
-                                    .area(area)
-                                    .location(location)
-                                    .width(damageSetRequestDTO.getWidth())
-                                    .images(damageSetRequestDTO.getImages())
-                                    .memberId(damageSetRequestDTO.getMemberId())
-                                    .build();
-
-                            serviceDTO.setHexagonIndex(hexagonIndex);
-                            iDamageService.setDamage(serviceDTO);
-                        } catch (Exception e) {
-                            for (String s : newFileUrls)
-                                awsService.deleteFile(s);
-                            log.error("Error processing Kakao data: {}", e.getMessage());
-                            throw new PotholeNotFoundException();
-                        }
-                    });
 
         } catch (DuplPotholeException e) {
             // Duplicate pothole exception handling
             log.error("Duplicate pothole exception: {}", e.getMessage());
+        } catch(PotholeDetectionFailException e){
+            log.error("Pothole detection failed exception: {}", e.getMessage());
         } catch (WebClientResponseException e) {
             log.error("WebClient error: Status code {} - Body {}", e.getRawStatusCode(), e.getResponseBodyAsString());
         } catch (IOException e) {
