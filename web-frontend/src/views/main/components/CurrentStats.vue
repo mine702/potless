9497231a22f3201c3784e89ef22052d84cde83c1
wonnyboi <wjdgnldnjs97@gmail.com>
2024-07-15 09:31:13 +0900

    fix : set date to 240712 to show data constantly

--- web-frontend/src/views/main/components/CurrentStats.vue
+++ web-frontend/src/views/main/components/CurrentStats.vue
@@ -50,9 +50,12 @@ const { accessToken, areaId } = storeToRefs(store);
 const currentData = ref([]);
 const potholeNum = ref(0);
 const now = new Date();
-const year = now.getFullYear();
-const month = ("0" + (now.getMonth() + 1)).slice(-2);
-const day = ("0" + now.getDate()).slice(-2);
+// const year = now.getFullYear();
+// const month = ("0" + (now.getMonth() + 1)).slice(-2);
+// const day = ("0" + now.getDate()).slice(-2);
+const year = 2024;
+const month = 7;
+const day = 12;
 const today_start = year + "-" + month + "-" + (day - 1);
 const today_end = year + "-" + month + "-" + day;
 
